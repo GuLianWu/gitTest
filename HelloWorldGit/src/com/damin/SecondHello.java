46c59032702a53020e93e002b3a9856bcfc3704c
damin <damin@qq.com>
2019-11-04 10:50:31 +0800

    secondHello

---
+++ HelloWorldGit/src/com/damin/SecondHello.java
@@ -0,0 +1,12 @@
+package com.damin;
+
+/**
+ * ${Description}
+ *
+ * @author damin on 2019/11/4
+ */
+public class SecondHello {
+    public static void main(String[] args) {
+        System.out.println("second");
+    }
+}
