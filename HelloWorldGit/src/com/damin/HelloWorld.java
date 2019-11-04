8ad9262201744f2ad78579c2a3b25f75f9b55bf5
damin <damin@qq.com>
2019-11-04 10:40:04 +0800

    提交HelloWorld工程到本地

---
+++ HelloWorldGit/src/com/damin/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.damin;
+
+/**
+ * ${Description}
+ *
+ * @author damin on 2019/11/4
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("111111");
+    }
+}
